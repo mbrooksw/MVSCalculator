467df80872da76deb47d34c37d537489a39f668f
mbrooksw <bwmf51@gmail.com>
2024-07-28 19:10:23 -0400

    Update mean_var_std.py
    changed some method calls

--- mean-variance-standard-deviation-calculator/mean_var_std.py
+++ mean-variance-standard-deviation-calculator/mean_var_std.py
@@ -50,25 +50,30 @@ def sum_value(np_matrix):
 
 
 def calculate(list):
+    # create the dictionary to be returned
+    calculation = {
+        "mean": [],
+        "variance": [],
+        "standard deviation": [],
+        "max": [],
+        "min": [],
+        "sum": [],
+    }
 
     if len(list) != 9:
         # check if the length of list is greater than or less than 9
         # raise an error if length of list is not equal to 9
         raise ValueError("List must contain nine numbers.")
     else:
-        # create the dictionary to be returned
-        calculation = dict(
-            mean="", variance="", standarddeviation="", max="", min="", sum=""
-        )
         # convert list to numpy matrix
         np_matrix = np.array(list).reshape(3, 3)
 
-        # calculate & assign the valeus for the keys in the dictionary
-        calculation["mean"] = mean_value(np_matrix)
-        calculation["variance"] = var_value(np_matrix)
-        calculation["standarddeviation"] = std_value(np_matrix)
-        calculation["max"] = max_value(np_matrix)
-        calculation["min"] = min_value(np_matrix)
-        calculation["sum"] = sum_value(np_matrix)
+    # calculate & assign the valeus for the keys in the dictionary
+    calculation["mean"].append(mean_value(np_matrix))
+    calculation["variance"].append(var_value(np_matrix))
+    calculation["standard deviation"].append(std_value(np_matrix))
+    calculation["max"].append(max_value(np_matrix))
+    calculation["min"].append(min_value(np_matrix))
+    calculation["sum"].append(sum_value(np_matrix))
 
     return calculation
